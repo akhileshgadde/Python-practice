dc2fd8ecefb4ecdfc4760f1f52732753159ba139
Akhilesh Gadde <akhilesh.gadde@outlook.com>
2015-03-14 13:55:27 -0400

    pygLatin.py
    Pig Latin is a language game, where you move the first letter of the word to the end and add "ay if the first letter is a vowel, else add "ay" to the complete string, if the first letter is a consonant. So "aython" becomes "ythonpay" or "python" becomes "pythonay". 
    To write a Pig Latin translator in Python, here are the steps we'll need to take:
    1. Ask the user to input a word in English.
    2. Make sure the user entered a valid word.
    3. Convert the word from English to Pig Latin.
    4. Display the translation result.
    
    Credits:
    -----------
    CodeAcademy

---
+++ Pyglatin.py
@@ -0,0 +1,18 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    print original
+    word = original.lower()
+    first = word[0]
+    if first=="a" or first =="e" or first=="i" or first=="o" or first=="u":
+        print "Translation of a string beginning with vowel"
+        new_word = word+pyg
+        print new_word
+    else:
+        print "Translation of a string beginning with consonant"
+        new_word = word[1:len(word)] + first + pyg
+        print new_word
+else:
+    print "Empty or Invalid string: %s" % (original)
